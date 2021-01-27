ac872f5c88c069dbcc1c32b6c5267b117456fba8
shj3497 <shj3497@gmail.com>
2021-01-27 13:36:41 +0900

    2021-01-27 13:36 데이터 긁어오고 저장하는 작업 시작

--- basic/basic_22.py
+++ basic/basic_22.py
@@ -29,3 +29,11 @@ for i in range(1, 11):
 print(temp)
 print(a)
 # 흠.. 출력은 됬는데 뭔가 찝찝함
+print()
+# 정답
+a = [1,2,3,4,5,6,7,8,9,10]
+for i in range(0,5):
+    temp = a[i]
+    a[i] = a[9-i]
+    a[9-i] = temp
+print(a)
